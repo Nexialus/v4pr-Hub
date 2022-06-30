acf24ae6e3c1aa78c6e25b293ae8275ce3504478
Nexialus <108095155+Nexialus@users.noreply.github.com>
2022-06-30 07:56:56 +0100

    Update V4pr Hub.lua

--- V4pr Hub.lua
+++ V4pr Hub.lua
@@ -1,32 +1,29 @@
 local plrName = game:GetService("Players").LocalPlayer.Name;
-local gameCheck = 1;
-local stopWhile = 0;
 local supportedGames = {
 	7047488135,  --Speed Run Simulator  |  https://www.roblox.com/games/7047488135/Speed-Run-Simulator
+	70005410,  --Blox Hunt  |  --https://www.roblox.com/games/70005410/Blox-Hunt-v2-7-25
 	0  --do not load
 }
 
-while supportedGames[gameCheck] ~= 0 and stopWhile == 0 do
-	if supportedGames[gameCheck] == game.PlaceId then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Nexialus/v4pr-Hub/main/Scripts/" .. game.PlaceId .. ".lua"))()
-		stopWhile = 1
-	else
-		gameCheck = (gameCheck + 1)
-	end
-end
 
-if supportedGames[gameCheck] == 0 then
-	game:GetService("StarterGui")
-		game.StarterGui:SetCore("ChatMakeSystemMessage", {
-			Text = "No Supported Game Found.";
-			Color = Color3.fromRGB(200, 50, 100);
-			Font = Enum.Font.SourceSansBold;TextSize = 30
-	})
-else
-	game:GetService("StarterGui")
+for _,v in supportedGames do
+	if v == game.PlaceId then
+		game:GetService("StarterGui")
 		game.StarterGui:SetCore("ChatMakeSystemMessage", {
 			Text = "Thank You For Using V4pr Hub, " .. plrName .. ".";
 			Color = Color3.fromRGB(100, 50, 200);
 			Font = Enum.Font.SourceSans;TextSize = 30
-	})
+		})
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/Nexialus/v4pr-Hub/main/Scripts/" .. game.PlaceId .. ".lua"))()
+	else
+		if v == 0 then
+			game:GetService("StarterGui")
+			game.StarterGui:SetCore("ChatMakeSystemMessage", {
+				Text = "No Supported Game Found.";
+				Color = Color3.fromRGB(200, 50, 100);
+				Font = Enum.Font.SourceSansBold;TextSize = 30
+			})
+			break
+		end
+	end
 end
