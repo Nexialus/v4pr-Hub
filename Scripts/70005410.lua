12ea5cdfb6d8ab9eeb71b53a176cae760b7b0767
Nexialus <108095155+Nexialus@users.noreply.github.com>
2022-06-30 09:29:44 +0100

    Create 70005410.lua

---
+++ Scripts/70005410.lua
@@ -0,0 +1,11 @@
+--Blox Hunt
+
+getgenv().tokenFarm
+getgenv().hiderAutoWin
+
+local plrHead = game:GetService("Workspace").Players.LocalPlayer.Head
+
+function(tokenFarmFunc()
+	spawn(function()
+		while tokenFarm and wait() do
+			)
