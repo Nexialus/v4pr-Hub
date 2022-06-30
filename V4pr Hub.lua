b81df80e4a25d5e1605ae7209895b5bba32cd084
Nexialus <108095155+Nexialus@users.noreply.github.com>
2022-06-30 09:20:59 +0100

    Update V4pr Hub.lua

--- V4pr Hub.lua
+++ V4pr Hub.lua
@@ -1,23 +1,21 @@
 local plrName = game:GetService("Players").LocalPlayer.Name;
 local supportedGames = {
 	7047488135,  --Speed Run Simulator  |  https://www.roblox.com/games/7047488135/Speed-Run-Simulator
-	70005410,  --Blox Hunt  |  --https://www.roblox.com/games/70005410/Blox-Hunt-v2-7-25
+	70005410,  --Blox Hunt  |  https://www.roblox.com/games/70005410/Blox-Hunt-v2-7-25
 	0  --do not load
 }
 
 
-for _,v in supportedGames do
+for _,v in pairs(supportedGames) do
 	if v == game.PlaceId then
-		game:GetService("StarterGui")
 		game.StarterGui:SetCore("ChatMakeSystemMessage", {
 			Text = "Thank You For Using V4pr Hub, " .. plrName .. ".";
-			Color = Color3.fromRGB(100, 50, 200);
+			Color = Color3.fromRGB(150, 70, 250);
 			Font = Enum.Font.SourceSans;TextSize = 30
 		})
 		loadstring(game:HttpGet("https://raw.githubusercontent.com/Nexialus/v4pr-Hub/main/Scripts/" .. game.PlaceId .. ".lua"))()
 	else
 		if v == 0 then
-			game:GetService("StarterGui")
 			game.StarterGui:SetCore("ChatMakeSystemMessage", {
 				Text = "No Supported Game Found.";
 				Color = Color3.fromRGB(200, 50, 100);
